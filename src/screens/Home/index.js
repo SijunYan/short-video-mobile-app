3e39ab186e62085ab282eebdcdbc6bf91946d585
SIJUN YAN <90133908+SijunYan@users.noreply.github.com>
2022-03-22 09:51:43 +1100

    init

--- src/screens/Home/index.js
+++ src/screens/Home/index.js
@@ -1,11 +1,9 @@
-import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
-import Home from "./src/screens/Home";
 
-export default function App() {
+export default function Home() {
   return (
     <View style={styles.container}>
-      <Home />
+      <Text>Home screen</Text>
     </View>
   );
 }
